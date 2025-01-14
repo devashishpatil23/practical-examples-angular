bd7a863487c122eb9c8470ec22560e89b31aaba3
Devashish satish patil <devashishpatil23@gmail.com>
2025-01-15 00:46:01 +0530

    link added

--- src/app/pages/host-listner/host-listner.component.ts
+++ src/app/pages/host-listner/host-listner.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HoverHighlightDirective } from '../../custom-directives/hover-highlight.directive';
-import { NoCopyNoTextSelectionDirective } from '../../custom-directives/no-copy-no-text-selection.directive';
-import { NoRightClickDirective } from '../../custom-directives/no-right-click.directive';
+import { HoverHighlightDirective } from '../../custom-directives/host-listner/hover-highlight.directive';
+import { NoCopyNoTextSelectionDirective } from '../../custom-directives/host-listner/no-copy-no-text-selection.directive';
+import { NoRightClickDirective } from '../../custom-directives/host-listner/no-right-click.directive';
 
 @Component({
   selector: 'app-host-listner',
